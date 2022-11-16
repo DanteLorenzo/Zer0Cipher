8f511351c731915b9c539f50757c34943d7daca8
D <n1emand4me@gmail.com>
2022-11-15 17:19:58 -0800

    v1.0

---
+++ Zer0Cipher.py
@@ -0,0 +1,73 @@
+import os
+import pyAesCrypt
+
+
+class Locker:
+
+    def __init__(self):
+        self.dir = os.getcwd()
+        self.password = ''
+        self.action = ''
+        self.exception_file = "Zer0Cipher.exe"
+        self.chooseAnAction()
+        self.checkPassword()
+
+    def crypt(self, file):
+        buffer_size = 512*1024
+        pyAesCrypt.encryptFile(str(file), str(file) + ".crp", self.password, buffer_size)
+        print("[Encrypt] '"+str(file)+".crp'")
+        os.remove(file)
+
+    def decrypt(self, file):
+
+        buffer_size = 512 * 1024
+        pyAesCrypt.decryptFile(str(file), str(os.path.splitext(file)[0]), self.password, buffer_size)
+        print("[Decrypt] '" + str(os.path.splitext(file)[0]) + "'")
+        os.remove(file)    
+
+    def run(self):
+
+        
+        for name in os.listdir(self.dir):
+            if name == self.exception_file:
+                continue
+            path = os.path.join(self.dir, name)
+            if os.path.isfile(path):
+                if self.action == 1:
+                    self.crypt(path)
+                elif self.action == 2:
+                    try:
+                        self.decrypt(path)
+                    except:
+                        print("Wrong Password")
+
+            else:
+                main_dir = self.dir
+                self.dir = path
+                self.run()
+                self.dir = main_dir
+
+    def chooseAnAction(self):
+        print("[1] Encrypt \n[2] Decrypt")
+        inputAction = int(input("Choose action: "))
+        if inputAction == 1:
+            self.action = 1
+        elif inputAction == 2:
+            self.action = 2                      
+        else:
+            print("No avaliable action")
+            return self.chooseAnAction()
+
+
+    def checkPassword(self):
+        checkpswd1 = str(input("Password: "))
+        checkpswd2 = str(input("Repeat Password: "))
+        if checkpswd1 == checkpswd2:
+            self.password = checkpswd2
+        else:
+            print("Not correct password check")
+            return self.checkPassword()
+
+if __name__ == "__main__":
+    lk = Locker()
+    lk.run()
